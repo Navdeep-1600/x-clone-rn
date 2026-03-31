7f980946c68716b8eca1407e5b0cc14eaef1af57
Navdeep-1600 <navdeepd212@google.com>
2026-03-31 09:52:11 +0530

    Added Schema models and prettier

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,4 +2,4 @@ import express from "express";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is up and running at PORT: 5001"))
+app.listen(5001, () => console.log("Server is up and running at PORT: 5001"));
