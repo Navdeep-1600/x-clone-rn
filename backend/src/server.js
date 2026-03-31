ade0fc9293986de415cdf9e28747a09a57a07056
Navdeep-1600 <navdeepd212@google.com>
2026-03-31 11:12:13 +0530

    fixed error handling for connection DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,18 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
-
 app.get("/", (req, res) => res.send("Hello from server"));
 
 const startServer = async () => {
-    try{
-        await connectDB();
-        app.listen(ENV.PORT, () => console.log("Server is up and running at PORT: ", ENV.PORT))
-    }catch(error){
+    await connectDB();
+
+    const server = app.listen(ENV.PORT, () => {
+        console.log('Server is up and running at PORT: ${ENV.PORT}')
+    });
+    server.on("error", (error) => {
         console.error("Failed to start server:", error.message);
         process.exit(1);
-    }
+    });
 };
 
 startServer();
